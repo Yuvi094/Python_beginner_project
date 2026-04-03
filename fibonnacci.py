280e40d058cae39024f52f5fd442fe81c2251b78
YuvrajSingh <yuvrajsingh11302011@gmail.com>
2026-04-03 11:25:28 +0530

    Update fibonnacci.py

--- fibonnacci.py
+++ fibonnacci.py
@@ -1,10 +1,19 @@
-
+# function to generate a fibonacci sequence up to n terms
 def generate_fibonacci(n):
     a = 0
     b = 1
-    " "
+
     print(f"Fibonacci sequence (first {n} numbers):")
+    
     for i in range(n):
-        print(a)
-        a, b = b, a + b
+       # Show the first no.
+       print (a)
+       # Print the next no.
+       c = a + b
+       # shift a to next number to be added in the next loop
+       a = b
+       # shift a to c so that new b is added in next loop
+       b = c
+
+# Call the function for 8 terms
 generate_fibonacci(8)
